1492476d9d690c4885ed6fa85fac657281251014
Isa-josep <isaurini1902@gmail.com>
2024-03-27 21:07:38 -0600

    DATA MODIFICADA AND BETA DASHBOARD

--- php/post_view.php
+++ php/post_view.php
@@ -1,17 +1,36 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../card1.css">
+    <title>Publicaciones</title>
+</head>
+<body>
+    <?php
     include("../conexion.php");
     $sql = "SELECT * FROM publicaciones";
     $result = $conn->query($sql);
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
-            echo "Título: " . $row["titulo"] . "<br>";
-            echo "Contenido: " . $row["contenido"] . "<br>";
-            echo "Autor ID: " . $row["autor_id"] . "<br>";
-            echo "<hr>";
+            ?>
+            <div class="card">
+                <img class="card-image" alt="Card Image" src="http://hidroponia.mx/wp-content/uploads/2015/06/aguacate.jpg">
+                <div class="card-content">
+                    <h2 class="card-title"><?php echo $row["titulo"]; ?></h2>
+                    <p class="card-text"><?php echo $row["contenido"]; ?></p>
+                    <div class="button-container">
+                        <button class="learn-more-button">Learn More</button>
+                    </div>
+                </div>
+            </div>
+            <?php
         }
     } 
     else {
         echo "No se encontraron publicaciones.";
     }
     $conn->close();
-?>
+    ?>
+</body>
+</html>
--- sql/database.sql
+++ sql/database.sql
@@ -35,6 +35,23 @@ CREATE TABLE publicaciones (
     FOREIGN KEY (autor_id) REFERENCES usuarios(user_id)
 );
 
+-- CREATE TABLE frutas_verduras (
+--     fv_id INT AUTO_INCREMENT PRIMARY KEY,
+--     nombre VARCHAR(100) UNIQUE,
+--     tipo ENUM('fruta', 'verdura'),
+--     autor_id INT,
+--     FOREIGN KEY (autor_id) REFERENCES usuarios(user_id)
+-- );
+
+-- -- Insertar algunas frutas y verduras
+-- INSERT INTO frutas_verduras (nombre, tipo, autor_id) VALUES 
+-- ('Manzana', 'fruta', 1),
+-- ('Plátano', 'fruta', 1),
+-- ('Naranja', 'fruta', 2),
+-- ('Zanahoria', 'verdura', 2),
+-- ('Lechuga', 'verdura', 3),
+-- ('Pepino', 'verdura', 3); 
+
 -- !!TEST DE INSERCION DE DATOS
 INSERT INTO publicaciones (titulo, contenido, autor_id) VALUES 
 ('Publicacion 1', 'SE SUPONDE QUE EL USUSARIO NORMAL NO PUEDE PUBLICAR, YA DEPUES CHACAOS ESTO', 1),
@@ -47,3 +64,6 @@ SELECT p.publicacion_id, p.titulo, p.contenido, u.nombre AS nombre_autor
 FROM publicaciones p
 JOIN usuarios u ON p.autor_id = u.user_id;
 
+
+-- QUERY PARA CONSULTAR EL NOMBRE DE LOS AUTORES DE LAS FRUTAS Y VERDURAS
+SELECT * FROM frutas_verduras WHERE autor_id = 3 AND tipo = 'fruta'|| tipo='verdura';
