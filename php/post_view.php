df5590fd3863ac3d829bcc5f5c6e02ea720c9f73
Isa-josep <isaurini1902@gmail.com>
2024-04-02 21:04:34 -0600

    Update CSS file path in post_view.php

--- php/post_view.php
+++ php/post_view.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../card1.css">
+    <link rel="stylesheet" href="../SRC_POSIBLES DE USAR/card1.css">
     <title>Publicaciones</title>
 </head>
 <body>
